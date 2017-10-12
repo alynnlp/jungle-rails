0747ab12d493cfeb6cd6a79bd3332979751e3508
Aileen Pang <aileenpang63@gmail.com>
2017-10-12 20:44:59 +0000

    created categories when img is clicked

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,30 @@
+#you need a controller for the routes you created at config/routes.rb
+class Admin::CategoriesController < Admin::BaseController
+
+  def index
+    @categories = Category.order(id: :asc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name
+    )
+  end
+
+end
